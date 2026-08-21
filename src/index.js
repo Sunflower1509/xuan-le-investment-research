ae67bc0363263226ecf99cf8e6d1bf946a3896bc
Sunflower1509 <72440024+Sunflower1509@users.noreply.github.com>
2026-08-21 15:51:01 +0700

    Load 21 Aug Daily Market View

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./data/research-data.js";
 import "./data/daily-insights.js";
 import "./data/daily-insights-20260819.js";
+import "./data/daily-insights-20260821.js";
 import "./data/trade-ledger.js";
 import "./scripts/app.js";
