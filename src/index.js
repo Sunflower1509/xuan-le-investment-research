6d4adc1e6339547cbeed32181e7fe409ce61f4e6
Sunflower1509 <72440024+Sunflower1509@users.noreply.github.com>
2026-08-27 08:20:11 +0700

    Load market view for 26 Aug 2026

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import "./data/research-data.js";
 import "./data/daily-insights.js";
 import "./data/daily-insights-20260819.js";
 import "./data/daily-insights-20260821.js";
+import "./data/daily-insights-20260826.js";
 import "./data/trade-ledger.js";
 import "./scripts/app.js";
