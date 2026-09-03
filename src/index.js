190ceae9d7a04a9190ef20e47cc2b589f58f314e
Sunflower1509 <72440024+Sunflower1509@users.noreply.github.com>
2026-09-03 13:20:28 +0700

    Wire stock cover thumbnails into production bundle

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ import "./data/daily-insights-20260821.js";
 import "./data/daily-insights-20260826.js";
 import "./data/trade-ledger.js";
 import "./scripts/app.js";
+import "./scripts/stock-thumbnails.js";
