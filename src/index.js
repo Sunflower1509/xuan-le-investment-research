194aa66596d2539bf7281dd57943d097380a086d
Sunflower1509 <72440024+Sunflower1509@users.noreply.github.com>
2026-09-11 19:25:21 +0700

    Load EOD market view for 2026-09-11

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./data/daily-insights.js";
 import "./data/daily-insights-20260819.js";
 import "./data/daily-insights-20260821.js";
 import "./data/daily-insights-20260826.js";
+import "./data/daily-insights-20260911.js";
 import "./data/trade-ledger.js";
 import "./scripts/app.js";
 import "./scripts/company-logo-avatars.js";
